27ad3b72b76358b99f0b3e1956e0b9936dc20988
ZnasTo <99986566+ZnasTo@users.noreply.github.com>
2023-04-14 09:35:27 +0200

    pridani struktury spravy, chybi dodelat komunikace s databazi

--- View/checkout.php
+++ View/checkout.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
+    require("htmlTop.php");
     // require_once("../Model/vendor/autoload.php");
     // require_once("../Controller/Controller.php");
 
@@ -20,6 +13,6 @@
 <form action="<?=$url?>" method="post">
   <button name="pay" type="submit">Zaplatit</button>
 </form>
-    
-    </body>
-</html>
+<?php
+require("htmlBottom.html");
+?>
