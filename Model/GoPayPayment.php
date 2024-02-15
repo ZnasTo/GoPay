ddb5aa3ad6c1968ad8680ebff0da92cd876d6b6f
KeodaCZE <ninja123456789nm@seznam.cz>
2024-02-15 09:48:50 +0100

    fix spatneho merge

--- Model/GoPayPayment.php
+++ Model/GoPayPayment.php
@@ -80,91 +80,87 @@ class GoPayPayment extends Payment
        $this->initialisePayment();
 
        $this->response = $this->token->createPayment([
-        'payer' => [
-            'default_payment_instrument' => PaymentInstrument::PAYMENT_CARD, 
-            //     'default_payment_instrument' => PaymentInstrument::BANK_ACCOUNT,
-            'allowed_payment_instruments' => [PaymentInstrument::PAYMENT_CARD],
-            //     'default_swift' => BankSwiftCode::FIO_BANKA,
-            'allowed_swifts' => [BankSwiftCode::FIO_BANKA, BankSwiftCode::MBANK],
-                'contact' => ['first_name' => $this->buyerData["jmeno"],
-                        'last_name' => $this->buyerData["prijmeni"],
-                        'email' => $this->buyerData["email"],
-                        'phone_number' => "+" . $this->buyerData["telefon"],
-                        'city' => $this->buyerData["mesto"],
-                        'street' => $this->buyerData["ulice"] . ' ' . $this->buyerData["CP"],
-                        'postal_code' => $this->buyerData["PSC"],
-                        'country_code' => 'CZE'
-                ]
-        ],
-        'amount' => $this->buyerData["castka"],
-        'currency' => Currency::CZECH_CROWNS,
-        'order_number' => $this->buyerData["cislo_objednavky"],
-    // 'order_description' => 'obuv',
-    // 'items' => [[ // asi uplně není pro nas podstatné
-    //         'type' => 'ITEM',
-    //         'name' => 'obuv',
-    //         'product_url' => 'https://www.eshop.cz/boty/lodicky',
-    //         'ean' => 1234567890123,
-    //         'amount' => 119990,
-    //         'count' => 1,
-    //         'vat_rate' => VatRate::RATE_4
-    // ],
-    //         [
-    //         'type' => PaymentItemType::ITEM,
-    //         'name' => 'oprava podpatku',
-    //         'product_url' => 'https://www.eshop.cz/boty/opravy',
-    //         'ean' => 1234567890189,
-    //         'amount' => 19961,
-    //         'count' => 1,
-    //         'vat_rate' => VatRate::RATE_3
-    //         ]],
-    // EET bylo v česku zrušeno
-    // ----------------------------------------
-    //     'eet' => [
-    //             'celk_trzba' => 139951,
-    //             'zakl_dan1' => 99160,
-    //             'dan1' => 20830,
-    //             'zakl_dan2' => 17358,
-    //             'dan2' => 2603,
-    //             'mena' => Currency::CZECH_CROWNS
-    //     ],
-    // ----------------------------------------
-    // 'additional_params' => [['name' => 'invoicenumber',
-    //         'value' => '2015001003'
-    // ]],
-        'callback' => [
-                // 'return_url' => 'http://localhost/GoPay/GoPay/View/checkout.php',
-                'return_url' => $this->returnURL,
-                'notification_url' => 'http://idkneco.wz.cz/notification_handler.php'
-        ],
-        'lang' => Language::CZECH
+            'payer' => [
+                'default_payment_instrument' => PaymentInstrument::PAYMENT_CARD, 
+                //     'default_payment_instrument' => PaymentInstrument::BANK_ACCOUNT,
+                'allowed_payment_instruments' => [PaymentInstrument::PAYMENT_CARD],
+                //     'default_swift' => BankSwiftCode::FIO_BANKA,
+                'allowed_swifts' => [BankSwiftCode::FIO_BANKA, BankSwiftCode::MBANK],
+                    'contact' => ['first_name' => $this->buyerData["jmeno"],
+                            'last_name' => $this->buyerData["prijmeni"],
+                            'email' => $this->buyerData["email"],
+                            'phone_number' => "+" . $this->buyerData["telefon"],
+                            'city' => $this->buyerData["mesto"],
+                            'street' => $this->buyerData["ulice"] . ' ' . $this->buyerData["CP"],
+                            'postal_code' => $this->buyerData["PSC"],
+                            'country_code' => 'CZE'
+                    ]
+            ],
+            'amount' => $this->buyerData["castka"],
+            'currency' => Currency::CZECH_CROWNS,
+            'order_number' => $this->buyerData["cislo_objednavky"],
+            // 'order_description' => 'obuv',
+            // 'items' => [[ // asi uplně není pro nas podstatné
+            //         'type' => 'ITEM',
+            //         'name' => 'obuv',
+            //         'product_url' => 'https://www.eshop.cz/boty/lodicky',
+            //         'ean' => 1234567890123,
+            //         'amount' => 119990,
+            //         'count' => 1,
+            //         'vat_rate' => VatRate::RATE_4
+            // ],
+            //         [
+            //         'type' => PaymentItemType::ITEM,
+            //         'name' => 'oprava podpatku',
+            //         'product_url' => 'https://www.eshop.cz/boty/opravy',
+            //         'ean' => 1234567890189,
+            //         'amount' => 19961,
+            //         'count' => 1,
+            //         'vat_rate' => VatRate::RATE_3
+            //         ]],
+            // EET bylo v česku zrušeno
+            // ----------------------------------------
+            //     'eet' => [
+            //             'celk_trzba' => 139951,
+            //             'zakl_dan1' => 99160,
+            //             'dan1' => 20830,
+            //             'zakl_dan2' => 17358,
+            //             'dan2' => 2603,
+            //             'mena' => Currency::CZECH_CROWNS
+            //     ],
+            // ----------------------------------------
+            // 'additional_params' => [['name' => 'invoicenumber',
+            //         'value' => '2015001003'
+            // ]],
+            'callback' => [
+                    // 'return_url' => 'http://localhost/GoPay/GoPay/View/checkout.php',
+                    'return_url' => $this->returnURL,
+                    'notification_url' => 'http://idkneco.wz.cz/notification_handler.php'
+            ],
+            'lang' => Language::CZECH
 
-    ]);
+        ]);
     
     
     
+    
+    }   
+
+    // Metoda pro inicializaci platby
+    private function initialisePayment(){
+        $this->token = (
+            GoPay\payments([
+                'goid' => GO_ID,
+                'clientId' => CLIENT_ID,
+                'clientSecret' => CLIENT_SECERET,
+                'gatewayUrl' => 'https://gw.sandbox.gopay.com/',
+                'scope' => GoPay\Definition\TokenScope::ALL,
+                'language' => GoPay\Definition\Language::CZECH,
+                'timeout' => 30
+            ])
+            );
+    }
+
     
 }
 
-// Metoda pro inicializaci platby
-private function initialisePayment(){
-    $this->token = (
-        GoPay\payments([
-            'goid' => GO_ID,
-            'clientId' => CLIENT_ID,
-            'clientSecret' => CLIENT_SECERET,
-            'gatewayUrl' => 'https://gw.sandbox.gopay.com/',
-            'scope' => GoPay\Definition\TokenScope::ALL,
-            'language' => GoPay\Definition\Language::CZECH,
-            'timeout' => 30
-        ])
-        );
-}
-
-    
-<<<<<<< HEAD
-}
-=======
-}
-
->>>>>>> origin/main
