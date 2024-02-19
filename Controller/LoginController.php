98045f6e318779f7537e0bf87c383b50c3cc7442
ZnasTo <99986566+ZnasTo@users.noreply.github.com>
2024-02-19 13:28:41 +0100

    zhezčení loginControlleru

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -4,31 +4,33 @@ class LoginController extends Controller{
     public function execute($parameters)
     {   
         // Zjistí jestli je uživatel přihlášen, pokud ano přesěruje ho na úvod
-        if($_SESSION["prihlasen"] == true)
+        if($_SESSION["prihlasen"] == true) {
             $this->redirect("mainpage");
-            $this->view = "login";
+        }
 
-            //Zpravá pro uživatele, když něco zadá špatně
-            $this->data["error"] = "";
+        $this->view = "login";
 
-            // generovani hesla
-            // $hashnuteHeslo = password_hash("test", PASSWORD_BCRYPT); // hashnuti hesla
-            // print($hashnuteHeslo);
+        //Zpravá pro uživatele, když něco zadá špatně
+        $this->data["error"] = "";
 
-            if(isset($_POST["jmeno"]) && isset($_POST["heslo"])){
-                $jmeno = $_POST['jmeno'];
-                $heslo = $_POST['heslo'];
-    
-                $login = new Login();
+        // generovani hesla
+        // $hashnuteHeslo = password_hash("test", PASSWORD_BCRYPT); // hashnuti hesla
+        // print($hashnuteHeslo);
 
-                // Ověří přihlašovací údaje, jinak vypíše error message
-                if($login->login($jmeno,$heslo)){
-                    $this->redirect("mainpage");
-                } else {
-                    $this->data["error"] = $login->getErrorMsg(); 
-                }
-     
+        if(isset($_POST["jmeno"]) && isset($_POST["heslo"])){
+            $jmeno = $_POST['jmeno'];
+            $heslo = $_POST['heslo'];
+
+            $login = new Login();
+
+            // Ověří přihlašovací údaje, jinak vypíše error message
+            if($login->login($jmeno,$heslo)){
+                $this->redirect("mainpage");
+            } else {
+                $this->data["error"] = $login->getErrorMsg(); 
             }
+    
+        }
         
 
 
