ae3b6d7caa15ce4f9d977ef5be4ef9a325e12eab
KeodaCZE <ninja123456789nm@seznam.cz>
2024-02-29 01:31:01 +0100

    vylepseni api controleru

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -19,22 +19,83 @@ class ApiController extends Controller {
                 break;
         }
 
-        if(isset($_GET["email"]) && isset($_GET["castka"])){    
-            $udaje = array();
-            foreach ($_GET as $key => $value) {
-                $udaje[$key] = $value;
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
+            $paymentState = $payment->getIformation($id);
+            //get transaction information (oddeleni) from our database
+            $query = "SELECT oddeleni, cislo_objednavky FROM transakce WHERE id_transakce = $paymentState[order_number]";
+            $vysledek = Db::queryOne($query);
+            if($vysledek == 1){
+                $this->redirect("error");
             }
-            $udaje["oddeleni"] = "platebni_brana";
-            $urlParameters["buyerData"] = $udaje;
-            $urlParameters["returnURL"] = "http://$_SERVER[HTTP_HOST]/PaymentDemo";
-            $urlParameters["notificationURL"] = "http://$_SERVER[HTTP_HOST]/Model/Notifications";
-            $url = $payment->getUrl($urlParameters);
+            else{
+                $oddeleni = $vysledek["oddeleni"];
+                $cislo_objednavky = $vysledek["cislo_objednavky"];
 
-            //print json_encode(["url" => $url]);
-            header("Location: $url");
-        }
-        else{
-            print "error";
+                $query = "SELECT url FROM oddeleni WHERE nazev = '$oddeleni'";
+                $url = Db::queryOne($query);
+                if($url == 1){
+                    $this->redirect("error");
+                }
+                else{
+                    if($oddeleni == "platebni_brana"){
+                        print "id=$cislo_objednavky&stav=$paymentState[state]";
+                    } else {
+                        $url = $url['url']."?id=$cislo_objednavky&stav=$paymentState[state]";
+                        header("Location: $url");
+                    }
+                }
+            }
+
+
+
+        } else {    
+            if(isset($_GET["email"]) && isset($_GET["castka"]) && isset($_GET["cislo_objednavky"])) {    
+                $udaje = array();
+                foreach ($_GET as $key => $value) {
+                    $udaje[$key] = $value;
+                }
+                //hodit data do database a vratit ID pokud nejsou nastaveny dat null
+                $query = Db::queryAndReturnId(
+                    "INSERT INTO transakce
+                    VALUES(NULL, '{$udaje['oddeleni']}',
+                    '".$udaje['jmeno']?$udaje['jmeno']:null."', '".$udaje['prijmeni']??null."', '{$udaje['email']}',
+                    '".$udaje['telefon']??null."', '".$udaje['mesto']??null."', '".$udaje['ulice']??null."', '".$udaje['CP']??null."', '".$udaje['PSC']??null."',
+                    '{$udaje['castka']}', NULL, NULL, NOW(), NULL,NULL, {$udaje['cislo_objednavky']}
+                    );"
+                );
+                print $query;
+                if(is_bool($query)) {
+                    $this->redirect("error");
+                } else {
+                    $udaje["cislo_objednavky"] = $query;
+                    $urlParameters["buyerData"] = $udaje;
+                    $urlParameters["returnURL"] = "http://$_SERVER[HTTP_HOST]/Api?brana=$platebniBrana";
+                    $urlParameters["notificationURL"] = "http://$_SERVER[HTTP_HOST]/Model/Notifications";
+                    $url = $payment->getUrl($urlParameters);
+                }
+
+                if(str_contains($url,"error")){
+                    //Získat url z spravneho oddělení z database
+                    $query = "SELECT url FROM oddeleni WHERE nazev = '$udaje[oddeleni]'";
+                    $url = Db::queryOne($query);
+                    if(is_bool($query)){
+                        $this->redirect("error");
+                    }
+                    else{
+                        $url = $url."?error=platbu_se_nepodařilo_vytvořit";
+                        header("Location: $url");
+                    }
+                } else {
+                    
+                    header("Location: $url");
+                }
+
+                //print json_encode(["url" => $url]);
+            }
+            else{
+                print "error";
+            }
         }
         
     }
