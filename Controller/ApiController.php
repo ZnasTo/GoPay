ad50749a97d093a203598b8ec504caae212edde7
KeodaCZE <ninja123456789nm@seznam.cz>
2024-03-28 12:31:08 +0100

    Uprava že se posílají data na různé oddělení přes backend

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -1,5 +1,7 @@
 <?php
 class ApiController extends Controller {
+    const API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MTE0NjY5MjgsImV4cCI6MTc0NzQ2NjkyOCwicm9sZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9EQVRBX1JFQUQiLCJST0xFX0RBVEFfQ1JFQVRFIiwiUk9MRV9EQVRBX0VESVQiLCJST0xFX0RBVEFfREVMRVRFIiwiUk9MRV9DSEFOR0VfT1JERVJfU1RBVEUiXSwiY3VzdG9tZXJfZW1haWwiOiJhZG1pbkBzcHNlaW9zdHJhdmEuY3oifQ.VD0SOKmkhDLq5kBfkQYYsnscbeg2oMzZE9ipf45s99uaSEqFEINREf2ZpBPGhU25u0X82I9CqKpDOImmsIPvCfgwwhTzuN_2e9jYcK64UKaMgH9mYpBzxOwhgBxiVFa8sl0E4ITo2TqUKvGQtiIq4yQuFmz8pvxqs3XNNkGlWezmOwyFLVn_errJsXENfObjXnc5eVj1Kp-GnhPCvfSLX_uQYu8MOpGlB3Smkq1YqV6H4h6n72-ROB21E3TgxQ1XjPdbVJtQx8Rrm-QTN5qchJ3GL5Oz-J0OqAmaePK6r13sc7ZQ3wRyKC9_sRPjm2EbeZsn-53xvtyRMedjo4dH1w";
+    
     public function execute($parameters)
     {
         if(isset($_GET["brana"])){
@@ -32,17 +34,40 @@ class ApiController extends Controller {
                 $oddeleni = $vysledek["oddeleni"];
                 $cislo_objednavky = $vysledek["cislo_objednavky"];
 
-                $query = "SELECT url FROM oddeleni WHERE nazev = '$oddeleni'";
+                $query = "SELECT url, notification_url FROM oddeleni WHERE nazev = '$oddeleni'";
                 $url = Db::queryOne($query);
                 if($url == 1){
                     $this->redirect("error");
                 }
                 else{
                     if($oddeleni == "platebni_brana"){
-                        print "id=$cislo_objednavky&stav=$paymentState[state]";
+                        print "id=$cislo_objednavky&stav=$paymentState[state]&castka=$paymentState[amount]";
                     } else {
-                        $url = $url['url']."?id=$cislo_objednavky&stav=$paymentState[state]";
-                        header("Location: $url");
+                        //send data to the correct department
+                        //send token in the header
+                        $ch = curl_init();
+                        curl_setopt($ch, CURLOPT_URL, $url['notification_url']);
+                        curl_setopt($ch, CURLOPT_POST, 1);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, 
+                            HTTP_BUILD_QUERY(array(
+                                "id" => $cislo_objednavky, 
+                                "stav" => $paymentState['state'], 
+                                "castka" => $paymentState['amount']
+                            ))
+                        );
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                            'Authorization: Bearer '. self::API_TOKEN
+                        ));
+                        $response = curl_exec($ch);
+                        curl_close($ch);
+                        var_dump($response);
+
+                        //redirect to the correct department
+                        $redirectURL = $url['url']."?".http_build_query(array(
+                            "id" => $cislo_objednavky
+                        ));
+                        header("Location: $redirectURL");
                     }
                 }
             }
@@ -74,16 +99,16 @@ class ApiController extends Controller {
                 
 
                 //hodit data do database a vratit ID pokud nejsou nastaveny dat null
-                /*$query = Db::queryAndReturnId(
+                $query = Db::queryAndReturnId(
                     "INSERT INTO transakce
                     VALUES(NULL, '{$udaje['oddeleni']}',
                     '{$udaje['jmeno']}', '{$udaje['prijmeni']}', '{$udaje['email']}',
                     '{$udaje['telefon']}', '{$udaje['mesto']}', '{$udaje['ulice']}', '{$udaje['CP']}', '{$udaje['PSC']}',
                     '{$castka}', NULL, NULL, NOW(), NULL,NULL, {$udaje['cislo_objednavky']}
                     );"
-                );*/
+                );
 
-                $query = DB::queryAndReturnId(
+                /*$query = DB::queryAndReturnId(
                     'INSERT INTO transakce VALUES(NULL,?,?,?,?,?,?,?,?,?,?,?,?,NULL,NULL,NOW(),NULL,?,?)',
                     [
                         $udaje['oddeleni'],
@@ -98,7 +123,7 @@ class ApiController extends Controller {
                         $udaje['castka'],
                         $request,
                         $udaje['cislo_objednavky']
-                    ]);
+                    ]);*/
 
                 //print $query;
                 if(is_bool($query)) {
@@ -119,7 +144,7 @@ class ApiController extends Controller {
                         //$this->redirect("error");
                     }
                     else{
-                        $url = $url."?error=platbu_se_nepodařilo_vytvořit";
+                        $url = $url."?id='{$udaje['cislo_objednavky']}'&error=platbu_se_nepodařilo_vytvořit";
                         header("Location: $url");
                     }
                 } else {
@@ -130,7 +155,22 @@ class ApiController extends Controller {
                 //print json_encode(["url" => $url]);
             }
             else{
-                print "error";
+                //print "error";
+                if(isset($_GET["oddeleni"])){
+                    $oddeleni = $_GET["oddeleni"];
+                    $query = "SELECT url FROM oddeleni WHERE nazev = '$oddeleni'";
+                    $url = Db::queryOne($query);
+                    if($url == 1){
+                        $this->redirect("error");
+                    }
+                    else{
+                        $url = $url['url']."?error=nebyly_vyplněny_všechny_údaje";
+                        header("Location: $url");
+                    }
+                }
+                else{
+                    $this->redirect("error");
+                }
             }
         }
         
