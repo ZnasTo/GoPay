7027b9e9cfa82499fa335d4008628df4830926e5
KeodaCZE <ninja123456789nm@seznam.cz>
2024-03-29 15:35:53 +0100

    Úprava přesměrování z department na showtoken

--- Controller/DepartmentController.php
+++ Controller/DepartmentController.php
@@ -5,12 +5,6 @@ class DepartmentController extends Controller
         if($_SESSION["prihlasen"] == true){
             $this->view = "department";
             $this->data["oddeleni"] = Db::queryAll("SELECT * FROM oddeleni");
-
-            // Přesměrování na výpis api_tokenu
-            if (isset($_GET["api_token"])) {
-                $oddeleni = $_GET["api_token"];
-                $this->redirect("showtoken?department=$oddeleni");
-            }
         }
         else{
             $this->redirect("login");
